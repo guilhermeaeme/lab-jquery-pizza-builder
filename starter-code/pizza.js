76a2c4eae266d5974424f4bca31aeb32985cae39
guilhermeaeme <guilhermeaeme@gmail.com>
2018-12-01 16:48:05 -0200

    rebuild

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,94 +1,51 @@
 // Write your Pizza Builder JavaScript in this file.
 
+let buttons = [
+	{ selector: '.btn-pepperonni', item: '.pep', className: null, text: 'pepperonni', price: 1 },
+	{ selector: '.btn-mushrooms', item: '.mushroom', className: null, text: 'mushrooms', price: 1 },
+	{ selector: '.btn-green-peppers', item: '.green-pepper', className: null, text: 'green peppers', price: 1 },
+	{ selector: '.btn-sauce', item: '.sauce', className: 'sauce-white', text: 'white sauce', price: 3 },
+	{ selector: '.btn-crust', item: '.crust', className: 'crust-gluten-free', text: 'gluten-free', price: 5 }
+];
+
 $(function(){
 	$('.btn-crust, .btn-sauce').removeClass('active');
 
 	$('.crust').removeClass('crust-gluten-free');
 	$('.sauce').removeClass('sauce-white');
 
-	$('.btn-pepperonni').click(function(e){
-		e.preventDefault();
-		$(this).toggleClass('active');
-		$('.pep').toggle();
+	$.each(buttons, function(index, button){
+		$(button.selector).click(function(e){
+			e.preventDefault();
 
-		updatePrice();
-	});
+			$(this).toggleClass('active');
 
-	$('.btn-mushrooms').click(function(e){
-		e.preventDefault();
-		$(this).toggleClass('active');
-		$('.mushroom').toggle();
+			if(button.className) {
+				$(button.item).toggleClass(button.className);
+			} else {
+				$(button.item).toggle();
+			}
 
-		updatePrice();
-	});
-
-	$('.btn-green-peppers').click(function(e){
-		e.preventDefault();
-		$(this).toggleClass('active');
-		$('.green-pepper').toggle();
-
-		updatePrice();
-	});
-
-	$('.btn-sauce').click(function(e){
-		e.preventDefault();
-		$(this).toggleClass('active');
-		$('.sauce').toggleClass('sauce-white');
-
-		updatePrice();
-	});
-
-	$('.btn-crust').click(function(e){
-		e.preventDefault();
-		$(this).toggleClass('active');
-		$('.crust').toggleClass('crust-gluten-free');
-
-		updatePrice();
+			updatePrice();
+		});
 	});
 
 	function updatePrice() {
 		var price = 10;
 
 		$('.price ul li').each(function(){
-			if($(this).text().includes('pepperonni')) {
-				$(this).hide();
-				if($('.btn-pepperonni').hasClass('active')) {
-					$(this).show();
-					price += 1;
-				}
-			}
+			var $that = $(this);
 
-			if($(this).text().includes('mushrooms')) {
-				$(this).hide();
-				if($('.btn-mushrooms').hasClass('active')) {
-					$(this).show();
-					price += 1;
-				}
-			}
+			$.each(buttons, function(index, button){
+				if($that.text().includes(button.text)) {
+					$that.hide();
 
-			if($(this).text().includes('green peppers')) {
-				$(this).hide();
-				if($('.btn-green-peppers').hasClass('active')) {
-					$(this).show();
-					price += 1;
+					if($(button.selector).hasClass('active')) {
+						$that.show();
+						price += button.price;
+					}
 				}
-			}
-
-			if($(this).text().includes('white sauce')) {
-				$(this).hide();
-				if($('.btn-sauce').hasClass('active')) {
-					$(this).show();
-					price += 3;
-				}
-			}
-
-			if($(this).text().includes('gluten-free')) {
-				$(this).hide();
-				if($('.btn-crust').hasClass('active')) {
-					$(this).show();
-					price += 5;
-				}
-			}
+			});
 		});
 
 		$('.price strong').text('$' + price);
